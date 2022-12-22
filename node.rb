1727e40e0a9ee1cc7c0cd65a6755b766d36c3fef
Yining <ynzhang23@gmail.com>
2022-12-22 17:04:44 +0800

    Fix the formatting

--- node.rb
+++ node.rb
@@ -4,10 +4,9 @@ require 'pry-byebug'
 
 # Creates a node for the binary tree
 class Node
-  include Comparable
-  attr_reader :value, :left, :right
+  attr_reader :value, :left_node, :right_node
 
-  def initalize(value, left_node, right_node)
+  def initialize(value, left_node = nil, right_node = nil)
     @value = value
     @left_node = left_node
     @right_node = right_node
