e737e5bb7c0c3e74573471236ac38bf32bbb74b7
Yining <ynzhang23@gmail.com>
2022-12-22 14:42:38 +0800

    Create initialize method

--- node.rb
+++ node.rb
@@ -5,6 +5,11 @@ require 'pry-byebug'
 # Creates a node for the binary tree
 class Node
   include Comparable
+  attr_reader :value, :left_node, :right_node
 
-
+  def initalize
+    @value = nil
+    @left_node = nil
+    @right_node = nil
+  end
 end
