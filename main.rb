a06f287b5c7c569297a3d7e6bbc8d9d4c0a6114e
Yining <ynzhang23@gmail.com>
2022-12-23 10:52:18 +0800

    Set up test file

--- main.rb
+++ main.rb
@@ -3,3 +3,31 @@
 require_relative 'node'
 require_relative 'tree'
 require 'pry-byebug'
+
+array = Array.new(15) { rand(1..100) }
+tree = Tree.new(array)
+tree.pretty_print
+p tree.balanced?
+p tree.pre_order
+p tree.in_order
+p tree.post_order
+
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.insert(rand(100..500))
+tree.pretty_print
+p tree.balanced?
+
+p tree.rebalance
+tree.pretty_print
+p tree.balanced?
+p tree.balanced?
+p tree.pre_order
+p tree.in_order
+p tree.post_order
